62a6e2dfa6afe0bff630785f37413e42be6bb20a
Shantharam Shenoy <21194336+shantharamshenoy@users.noreply.github.com>
2026-04-19 19:47:53 +0530

    asda

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,11 @@
-//@ts-ignore
+// @ts-ignore
 import { createApp } from "vinxi";
+// @ts-ignore
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-export default createApp(
-  tanstackStart()
-);
+// @ts-ignore
+export default createApp(tanstackStart({
+  server: {
+    preset: "node-server",
+  },
+}));
